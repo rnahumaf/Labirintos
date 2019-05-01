95d0d2bed4eec4a97d2e4e7fd50d15b3f9e14840
Rodrigo Nahum <34604938+rnahumaf@users.noreply.github.com>
2019-05-01 19:29:47 -0300

    Update LabirintoBasico_Validation.R

--- LabirintoBasico_Validation.R
+++ LabirintoBasico_Validation.R
@@ -1,15 +1,33 @@
-# Stress test (Use "m" resultante do script "LabirintoBasico")
+# Stress test
 
 # loops
-loops <- 10^5
+loops <- 10^5.5
 
 # Início
-atual_i <- altura - 1
-atual_j <- 2
+atual_i <- origem_i # Empresta do script anterior
+atual_j <- origem_j # Empresta do script anterior
+
+# Definir estratégias
+Estrategia <- function(E){
+  if(E == "A"){
+    Estrategia_A <<- T # aumenta o valor do caminho sobre o qual passa em 1
+    Estrategia_B <<- F
+    Estrategia_C <<- F
+  }
+  if(E == "B"){
+    Estrategia_A <<- F
+    Estrategia_B <<- T # cada passo tem um valor crescente em 1
+    Estrategia_C <<- F
+  }
+}
+Estrategia("B")
 
 # Trash variables
-Step_count <- 0
 m2 <- m
+Counts <- 0
+Step_count <- 3
+
+# S T A R T
 
 for(i in 1:loops){
 
@@ -28,24 +46,39 @@ for(i in 1:loops){
   }
   
   # Checar se movimento é válido
-  # (atual, atual+move, atual+[move/2])
-  # (1, 0, 1) NOT -> repeat
   if((atual_i + Move[1]) %in% 1:altura && (atual_j + Move[2]) %in% 1:largura){
-    # (1, 0, 1) REPEAT
+    
+    # (0, >=1) = inválido (caminho não deve atravessar paredes)
     if(m2[(atual_i+Move[1]/2), (atual_j+Move[2]/2)]==0 && m[(atual_i+Move[1]), (atual_j+Move[2])]>=1){
       next
     } else {
-      m2[(atual_i+Move[1]), (atual_j+Move[2])] <- 1 + Step_count
-      m2[(atual_i+Move[1]/2), (atual_j+Move[2]/2)] <- 1 + Step_count
-      atual_i <- atual_i+Move[1]
-      atual_j <- atual_j+Move[2]
-      Step_count <- Step_count + 1
-      next
+      
+      # Estratégias
+      if(Estrategia_A){ # aumenta o valor do caminho sobre o qual passa em 1
+        m2[(atual_i+Move[1]), (atual_j+Move[2])] <- 1 + m2[(atual_i+Move[1]), (atual_j+Move[2])]
+        m2[(atual_i+Move[1]/2), (atual_j+Move[2]/2)] <- 1 + m2[(atual_i+Move[1]), (atual_j+Move[2])]
+        atual_i <- atual_i+Move[1]
+        atual_j <- atual_j+Move[2]
+        next
+      }
+      if(Estrategia_B){ # cada passo tem um valor crescente em 1
+        m2[(atual_i+Move[1]), (atual_j+Move[2])] <- 1 + Step_count
+        m2[(atual_i+Move[1]/2), (atual_j+Move[2]/2)] <- 1 + Step_count
+        atual_i <- atual_i+Move[1]
+        atual_j <- atual_j+Move[2]
+        Step_count <- Step_count + 1
+        next
+      }
     }
-  } else {
-    next
   }
-}
+} 
 
-colfunc <- colorRampPalette(c(rgb(1,1,1,1),rgb(1,0,1,1), rgb(0,0,0,1)), alpha=TRUE, bias = 0.5)
-image(t(apply(m2, 2, rev)), asp = 1, axes=FALSE, col = colfunc(256))
+
+colfunc <- colorRampPalette(c(rgb(1,1,1,1),
+                              rgb(1,0,1,1),
+                              rgb(1,1,0,1),
+                              rgb(0,1,1,1),
+                              rgb(0,0,1,1), 
+                              rgb(0,1,0,1),
+                              rgb(1,0,0,1)), alpha=TRUE, bias = 1)
+image(t(apply(m2, 2, rev)), asp = altura/largura, axes=FALSE, col = colfunc(1000));
